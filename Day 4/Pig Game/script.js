c2d465a4b4b9a3b9d7d8eced89b9a706b610175d
imkai-177 <kayesalmoon84@gmail.com>
2025-12-08 21:30:48 +0600

    initial commit

--- Day 4/Pig Game/script.js
+++ Day 4/Pig Game/script.js
@@ -1,1 +1,85 @@
 'use strict';
+
+const btnDice = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
+
+const diceEl = document.querySelector('.dice');
+const player1 = document.querySelector('.player--0');
+const player2 = document.querySelector('.player--1');
+
+let currentScore, activePlayer, scores, playing;
+
+const init = function () {
+  currentScore = 0;
+  activePlayer = 0;
+  scores = [0, 0];
+  playing = true;
+
+  document.getElementById('score--0').textContent = 0;
+  document.getElementById('score--1').textContent = 0;
+  document.getElementById('current--0').textContent = 0;
+  document.getElementById('current--1').textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player1.classList.remove('player--winner');
+  player2.classList.remove('player--winner');
+  player1.classList.add('player--active');
+  player2.classList.remove('player--active');
+};
+
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player1.classList.toggle('player--active');
+  player2.classList.toggle('player--active');
+};
+
+// Roll the dice
+btnDice.addEventListener('click', function () {
+  if (playing) {
+    // Generate a number on dice
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // Display the dice roll
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // Check the dice
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+// Hold Button
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // Total Score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 10) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+// Reset Button
+btnNew.addEventListener('click', init);
